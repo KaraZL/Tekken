3917513920ba92b315d492781b2f2986e1376899
KaraZL <karazl.msnet@gmail.com>
2022-06-15 17:36:38 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,19 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+      resource_group_name = "terraform-storage-rg"
+      storage_account_name = "tekkenterraformstorage"
+      container_name = "tfstatefiles"
+      key = "terraform.tfstate"
+      # #key = "terraform.tfstate"
+      # #key = "dev.terraform.tfstate"
+  }
+  
 }
+
+
 
 provider "azurerm" {
   features {
