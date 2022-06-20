bdfe86c3dd798becea5d3614977a743932dd5ad4
KaraZL <karazl.msnet@gmail.com>
2022-06-20 16:01:33 +0200

    azure keyvault

---
+++ src/services/CharactersAPI/Helpers/PrefixKeyVaultSecretManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace CharactersAPI.Helpers
+{
+    public class PrefixKeyVaultSecretManager : IKeyVaultSecretManager
+    {
+        private readonly string _prefix;
+
+        public PrefixKeyVaultSecretManager(string prefix)
+        {
+            _prefix = $"{prefix}-"; //TekkenApp-This--That
+        }
+
+        public string GetKey(SecretBundle secret)
+        {
+            return secret.SecretIdentifier.Name.Substring(_prefix.Length)
+                .Replace("--", ConfigurationPath.KeyDelimiter); //ConfigurationPath.KeyDelimiter = :
+        }
+
+        public bool Load(SecretItem secret)
+        {
+            return secret.Identifier.Name.StartsWith(_prefix);
+        }
+    }
+}
