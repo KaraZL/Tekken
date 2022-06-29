36a6f7ab8de3433e0ccf392583af76bef69f27b5
KaraZL <karazl.msnet@gmail.com>
2022-06-29 14:53:34 +0200

    update terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,17 @@ terraform {
   required_providers{
       azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=3.11.0"
+    }
+
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "=2.25.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "=3.3.2"
     }
   }
 
